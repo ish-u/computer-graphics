7fbbfd085548da062c48bd1107e9c8fd14ed3396
Anmol Gupta <anmolgupta520@gmail.com>
2022-02-08 23:11:40 +0530

    added Bresenham's Algorithms

--- DDA_Algorithm/dda.cpp
+++ DDA_Algorithm/dda.cpp
@@ -9,13 +9,13 @@ void DDA(int x1, int y1, int x2, int y2)
     int dy = (y2 - y1);
     float m = (float)dy / dx;
 
-    // calculating the stesps - number of pixels that our algorithm need to write
+    // calculating the steps - number of pixels that our algorithm need to write
     int steps = abs(dx) > abs(dy) ? abs(dx) : abs(dy);
 
     /*
-        increment can be calulated by 
+        increment can be calulated by
             =>  inc_x = dx/abs(steps)
-            =>  inc_y = dy/abs(steps) 
+            =>  inc_y = dy/abs(steps)
     */
     float inc_x = (float)dx / steps;
     float inc_y = (float)dy / steps;
@@ -45,20 +45,19 @@ int main()
     initgraph(&gd, &gm, NULL);
 
     // Drawing a Rectangle with Diagonals using DDA function
-
     // horizontal line
-    DDA(50, 50, 50, 100);
-    DDA(50, 100, 100, 100);
+    DDA(50, 50, 50, 400);
+    DDA(50, 400, 400, 400);
 
     // vertical line
-    DDA(50, 50, 100, 50);
-    DDA(100, 50, 100, 100);
+    DDA(50, 50, 400, 50);
+    DDA(400, 50, 400, 400);
 
     // positive slope
-    DDA(50, 50, 100, 100);
+    DDA(50, 50, 400, 400);
 
     // negative slope
-    DDA(100, 50, 50, 100);
+    DDA(400, 50, 50, 400);
 
     getch();
     closegraph();
